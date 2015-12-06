fd383e98f804d1881179290a245027a9b144679c
Yicheng Luo <ethanluoyc@gmail.com>
2015-12-06 17:24:32 +0800

    initial test develop on my own copy of dotfiles

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,10 @@
 alias reload!='. ~/.zshrc'
+alias zshconfig='vim ~/.zshrc'
+alias ohmyzsh="vim ~/.oh-my-zsh"
+alias pp="cd ~/dev/projects"
+alias matlab="/Applications/MATLAB_R2015a.app/bin/matlab -nosplash -nodesktop"
+alias e="emacsclient -t"
+alias ec="emacsclient -n"
+alias vim="emacsclient -t"
+alias vi="emacsclient -t"
+alias sa="source activate"
